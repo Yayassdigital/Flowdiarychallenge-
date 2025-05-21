dd3b359da332dae98e5106a73247be23e3696647
Yayassdigital <aryahaya57@gmail.com>
2025-05-21 20:54:29 +0100

    Update authen.js

--- authen.js
+++ authen.js
@@ -2,7 +2,7 @@ const form= document.getElementById(loginform);
 form.addEventListener('Submit',function(e));
 
 const email= document.getElementById(email).ariaValueMax;
-const password= document.getElementById(email).ariaValueMax;
+const password= document.getElementById(password).ariaValueMax;
 console.log('email, email')
 console.log('password, passsword')
 
